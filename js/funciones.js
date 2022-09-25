92568bf0abb8c60756b4bb5dfe2b8c2e0ee4caa1
Francisco <francis_oli92@hotmail.com>
2022-09-25 14:02:17 -0300

    opciones+jquery

--- js/funciones.js
+++ js/funciones.js
@@ -1,51 +1,36 @@
-$("#guardar").click(function(){
-  alert("hola")
-})
-//funcion florencia
-var opcion_1 = new Array("YAD", "YHI", "SCLD", "SCLI", "FD", "FI");
-var opcion_2 = new Array("MSD", "MSI");
-var opcion_3 = new Array("MSD", "MSI", "FD", "FI");
+// var opcion_1 = new Array("YAD", "YHI", "SCLD", "SCLI", "FD", "FI");
+// var opcion_2 = new Array("MSD", "MSI");
+// var opcion_3 = new Array("MSD", "MSI", "FD", "FI");
 
-function seleccionar() {
-var acceso;
-acceso = document.RegistroPacientes.acceso[document.RegistroPacientes.acceso.selectedIndex].value;
-if (acceso != 0) {
-  mis_opts = eval("opcion_" + acceso);
-  num_opciones = mis_opts.length;
-  document.RegistroPacientes.opciones.length = num_opciones;
-  for (i = 0; i < num_opciones; i++) {
-    document.RegistroPacientes.opciones.options[i].value = mis_opts[i];
-    document.RegistroPacientes.opciones.options[i].text = mis_opts[i];
-  }
-} else {
-  document.RegistroPacientes.opciones.length = 0;
-  document.RegistroPacientes.opciones.value.options[0].value = "-"
-  document.RegistroPacientes.opciones.value.options[0].text = "-"
-
-}
-document.RegistroPacientes.opciones.options[0].selected = true;
-}
-//textarea
-
-
-
-//jquery para la diferencia
-$("#sale")
-.keyup(function() {
-  //var value = $( this ).val();
+$(document).ready(function () {
+  $("#sale").keyup(function() {
+  //console.log("hola")
+  // var value = $( this ).val();
   // var value=$('#in').val()-$('#out').val()
-  $("#dif").val($('#entra').val() - $('#sale').val());
+  $("#dif").val($('#sale').val()-$('#entra').val());
 })
-.keyup();
 
+$("#entra").keyup(function() {
+    //var value = $( this ).val();
+    // var value=$('#in').val()-$('#out').val()
+    $("#dif").val(  );
+  })
 
-$("#entra")
-.keyup(function() {
-  //var value = $( this ).val();
-  // var value=$('#in').val()-$('#out').val()
-  $("#dif").val($('#entra').val() - $('#sale').val());
-})
-.keyup();
+  $("#acceso").change(function () { 
+    var acceso=$(this).val()
+    console.log(acceso)
+    if(acceso==0){
+      $("#op").html("<option value='0' style='background-color:red;'>Seleccione tipo de acceso!</option>")
+    }else if(acceso==1){
+      $("#op").html("<option value='1'>YAD</option><option value='2'>YHI</option><option value='3'>SCLD</option><option value='4'>SCLI</option><option value='5'>FD</option><option value='6'>FI</option>")
+    }else if(acceso==2){
+      $("#op").html("<option value='7'>MSD</option><option value='8'>MS</option>")
+    }else{
+      $("#op").html("<option value='7'>MSD</option><option value='8'>MS</option><option value='5'>FD</option><option value='6'>FI</option>")
+    }
+  });
+  //$("#size").html("<option value='test'>item1: test 1</option><option value='test2'>item1: test 2</option>");
+});
 function setTwoNumberDecimal() {
   this.value = parseFloat(this.value).toFixed(3);
 }
